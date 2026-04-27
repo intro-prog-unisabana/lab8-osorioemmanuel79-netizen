a934426d08a39c3d7ce28bd652b55d1ef9a84f80
Emmanuel Osorio Bustacara <osorioemmanuel79@gmail.com>
2026-04-26 19:12:29 -0500

    questv3

--- question1.py
+++ question1.py
@@ -20,6 +20,8 @@ try:
     if num_supports == 0:
         print("Error: Cannot divide by zero! Supports must be greater than zero.")
     else:
-        load_per_support = total_load / num_supports
+        
         print(f"Load per support point: {load_per_support:.2f} N")
 
+except ValueError:
+    print("Error: Invalid input! Enter numeric values only.")
